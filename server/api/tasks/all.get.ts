878a8469fb986b66d07873e900940f1091b0bb85
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-11-09 20:12:14 -0300

    Inicio do desenvolvimento

---
+++ server/api/tasks/all.get.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  return await prisma.task.findMany({
+    where: {
+      userId: 0,
+    },
+  });
+});
