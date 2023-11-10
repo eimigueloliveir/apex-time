a77fd7f035b1be3af0d2771eac7cbd1946557ce6
paulohpps <paulohpps13@gmail.com>
2023-11-09 22:41:35 -0300

    Tela de login e instalação de libs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  plugins: [
+    { src: '~/plugins/fontawesome.ts' }
+  ],
+
+
 })
+module.exports = {
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ]
+}
