8b8957b83c257ce4f54dc140bce78bc90ebfa07b
paulohpps <paulohpps13@gmail.com>
2023-11-13 20:15:28 -0300

    modal cadastro tarefas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  ui: {
-    global: true,
-    icons: ['mdi', 'simple-icons']
-  },
-  plugins: [
-    { src: '~/plugins/fontawesome.ts' }
-  ],
-
-})
-module.exports = {
   css: [
     'bootstrap/dist/css/bootstrap.css',
     '@fortawesome/fontawesome-svg-core/styles.css'
-  ]
-}
+  ],
+  plugins: [
+    { src: '~/plugins/fontawesome.ts' }
+  ],
+})
