1799349729be9aca1e40980a8f9387629d218c5f
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-11-14 20:54:21 -0300

    correção vercel api server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui',"nuxt-auth-utils"],
+  modules: ["@nuxt/ui", "nuxt-auth-utils"],
   css: [
-    'bootstrap/dist/css/bootstrap.css',
-    '@fortawesome/fontawesome-svg-core/styles.css'
+    "bootstrap/dist/css/bootstrap.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
   ],
-  plugins: [
-    { src: '~/plugins/fontawesome.ts' }
-  ],
-})
+  plugins: [{ src: "~/plugins/fontawesome.ts" }],
+  nitro: {
+    preset: "vercel",
+  },
+});
